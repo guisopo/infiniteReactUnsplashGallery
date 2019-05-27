d242701ae0faecf22eac32135ad1366e682fa667
guisopo <solerguillermo@hotmail.com>
2019-05-27 11:08:26 +0200

    Fixbug: we need to concat the fetched data to the images array

--- client/src/components/Images.js
+++ client/src/components/Images.js
@@ -22,7 +22,7 @@ export class Images extends Component {
     this.setState({ start: this.state.start + count });
     axios
         .get(`/api/photos?count=${count}&start=${start}`)
-        .then(res => this.setState({ images: res.data }));
+        .then(res => this.setState({ images: this.state.images.concat(res.data) }));
   }
 
   render() {
