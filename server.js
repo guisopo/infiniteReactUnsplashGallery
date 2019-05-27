65c20f8aac52b9221cae4f4af12e263ad4f48a8d
guisopo <solerguillermo@hotmail.com>
2019-05-27 09:15:02 +0200

    Fixbug

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 global.fetch = require('node-fetch');
 const config = require('universal-config');
-const Unsplash = require('unsplash').default;
-const toJson = require('unsplash').toJson;
+const Unsplash = require('unsplash-js').default;
+const toJson = require('unsplash-js').toJson;
 const express = require('express');
 
 const unsplash = new Unsplash({
