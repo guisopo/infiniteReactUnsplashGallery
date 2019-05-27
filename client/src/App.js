c3ac5c049422cd1d95e8a9c47af6694ad8546f93
guisopo <solerguillermo@hotmail.com>
2019-05-27 09:54:20 +0200

    Create App structure

--- client/src/App.js
+++ client/src/App.js
@@ -3,11 +3,19 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>React App</h1>
-      </header>
-    </div>
+    <div id='root'>
+        <div className='hero is-fullheight is-bold is-info'>
+          <div className='hero-body'>
+            <div className='container'>
+              <div className='header content'>
+                <h1 className='title is-1'>
+                  Infinite Scroll by Unsplash
+                </h1>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
   );
 }
 
