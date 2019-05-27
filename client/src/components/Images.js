411ed629d406da29af34a728c888cb14ac712102
guisopo <solerguillermo@hotmail.com>
2019-05-27 10:19:32 +0200

    Fetch data whe component mount

--- client/src/components/Images.js
+++ client/src/components/Images.js
@@ -9,6 +9,13 @@ export class Images extends Component {
     start: 1
   };
 
+  componentDidMount() {
+    const { count, start } = this.state;
+    axios
+        .get(`/api/photos/count=${count}&start=${start}`)
+        .then(res => this.setState({ images: res.data}));
+  }
+
   render() {
     return(
       <div></div>
