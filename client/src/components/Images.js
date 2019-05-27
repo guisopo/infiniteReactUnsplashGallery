30812a3337c5c1b36e65932962d5898cb95b6641
guisopo <solerguillermo@hotmail.com>
2019-05-27 11:05:31 +0200

    Add InfiniteScroll and Image components and created method to fetch images from the api

--- client/src/components/Images.js
+++ client/src/components/Images.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import axios from 'axios';
 import InfiniteScroll from 'react-infinite-scroll-component';
+import Image from './Image';
 
 export class Images extends Component {
   state = {
@@ -16,11 +17,29 @@ export class Images extends Component {
         .then(res => this.setState({ images: res.data }));
   }
 
+  fetchImages = () => {
+    const { count, start } = this.state;
+    this.setState({ start: this.state.start + count });
+    axios
+        .get(`/api/photos?count=${count}&start=${start}`)
+        .then(res => this.setState({ images: res.data }));
+  }
+
   render() {
-    console.log(this.state);
     return(
-      <div>Hello</div>
-    )
+      <div className='images'>
+        <InfiniteScroll 
+          dataLength={this.state.images.length}
+          next={this.fetchImages}
+          hasMore={true}
+          loader={<h4>Loading...</h4>}
+        >
+          {this.state.images.map(image => (
+            <Image key={image.id} image={image} />
+          ))}
+        </InfiniteScroll>
+      </div>
+    );
   }
 }
 
