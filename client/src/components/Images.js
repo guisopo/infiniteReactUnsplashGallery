51f64b302d9b2b5095c85446a9cc5fcfdf52570d
guisopo <solerguillermo@hotmail.com>
2019-05-27 09:57:40 +0200

    Create Images component boilerplate

---
+++ client/src/components/Images.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+import InfinteScroll from 'react-infinite-scroll-component';
+
+export class Images extends Component {
+  render() {
+    return(
+      <div></div>
+    )
+  }
+}
+
+export default Images;
